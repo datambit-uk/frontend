68145287588f860b2aa856c1504812c2b1916faa
Doug Macdonald <macdonald.douglas@gmail.com>
2026-02-13 12:50:36 +0000

    Updated UI

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import PasswordReset from '../pages/ForgotPassword';
 import AccessRequest from '../pages/AccessRequest';
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend/',
   plugins: [react(),tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
